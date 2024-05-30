55f2d7f68be1113bb9d8f99c68d1bf1e9fc4c4e4
Akshit Tyagi <tyagiakshit100@gmail.com>
2024-05-30 23:42:06 +0530

    episode-2

--- App.js
+++ App.js
@@ -2,13 +2,16 @@
 //   const heading = React.createElement("h1", {}, "hello world form react"); // for one div 
 
 // nested div 
+import React from "react";
+import ReactDOM  from "react-dom/client";
+
 const parent = React.createElement(
     "div",
     { id: "parent" },[
     React.createElement(
       "div",
       { id: "child" },[
-      React.createElement("h1", {} , "hello akshit"),
+      React.createElement("h1", {} , "hello world "),
       React.createElement("h1", {} , "hello akshit r2")
     ]),
     React.createElement(
